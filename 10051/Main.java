b830ddfb4010967407361b157e27b123167f6078
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-18 17:43:30 -0400

    Major refactor to use a Block object. Commented out print and clearArrays method

--- 10051/Main.java
+++ 10051/Main.java
@@ -1,23 +1,50 @@
 import java.util.*;
 
+class Block {
+	int id;
+	int topFace;
+	Block parent;
+	
+	public Block(int id, int topFace) {
+		this.id = id;
+		this.topFace = topFace;
+	}
+	
+	@Override
+	public String toString() {
+		return id + " " + face(topFace);
+	}
+	
+	private static String face(int i) {
+		switch (i) {
+			case 0: return "front";
+			case 1: return "back";
+			case 2: return "left";
+			case 3: return "right";
+			case 4: return "top";
+		}
+		
+		return "bottom";
+	}
+}
+
 class Main {
 	//--This will hold the provided set of cubes
 	public static int[][] cubes = new int[501][6];
 	
 	//--This array will be used to build out the LIS
-	//--Dimensions are block position, color, and face
-	//--Value will be block ID
-	public static int[][][] stack = new int[501][101][6];
+	//--Dimensions are block position and color
+	public static Block[][] stack = new Block[501][101];
 	
-	//--parent[i] will return the id of the block that follows i
-	public static int[][] parent = new int[501][6];
 	public static int length;
 	public static int numCubes;
+	public static int caseNumber;
+	public static Block topBlock;
 	
 	public static void main(String[] args) {
 		Scanner conIn = new Scanner(System.in);
 		numCubes = conIn.nextInt();
-		int caseNumber = 0;
+		caseNumber = 0;
 		while (numCubes != 0) {
 			caseNumber++;
 			//--Get input
@@ -29,7 +56,7 @@ class Main {
 			
 			LIS();
 			
-			System.out.println(length);
+			printResults();
 					
 			numCubes = conIn.nextInt();
 			if (numCubes != 0) {
@@ -43,41 +70,68 @@ class Main {
 		length = 1;
 		//--add the heaviest block on its colors
 		for (int i = 0; i < 6; i++)
-			stack[1][cubes[1][i]][i] = numCubes;
+			stack[1][cubes[1][i]] = new Block(numCubes, opposite(i));
 			
 		//--for each lighter block
-		for (int i = numCubes - 1; i > 0; i--) {
+		for (int blockID = numCubes - 1; blockID > 0; blockID--) {
+		
 			//--see the stack has any blocks that have a top 
 			//--that matches this block's bottom
-			for (int j = length; j > 0; j--) {
-				for (int k = 0; k < 6; k++) {
-					for (int l = 0; l < 6; l++) {
-						if (stack[j][cubes[i][k]][l] != 0) {
-							//--add this block
-							stack[j + 1][cubes[i][k]][opposite(l)] = i;
-							//--set this block's parent
-							parent[i][k] = stack[j][cubes[i][k]][l];
-							if (j == length)
-								length++;
+			for (int position = length; position > 0; position--) {
+				for (int face = 0; face < 6; face++) {
+					if (stack[position][cubes[blockID][face]] != null) {
+						//--add this block
+						Block newBlock = new Block(blockID, opposite(face));
+						stack[position + 1][cubes[blockID][face]] = newBlock;
+						//--set this block's parent
+						newBlock.parent = stack[position][cubes[blockID][face]];
+						if (position == length) {
+							length++;
+							topBlock = newBlock;
 						}
 					}
 				}
 			}
+			
+			//--add all of it's faces to the first position
+			for (int face = 0; face < 6; face++)
+				stack[1][cubes[blockID][face]] = new Block(blockID, opposite(face));
 		}
 	}
 	
+	public static void printResults() {
+// 		System.out.println("Case #" + caseNumber);
+// 		System.out.println(length);
+// 		
+// 		while (length != 1) {
+// 			for (int i = 0; i < 6; i ++) {
+// 				if (parent[topBlock][length][i] != 0) {
+// 					System.out.println(topBlock + " " + face(i));
+// 					topBlock = parent[topBlock][length][i];
+// 					length--;
+// 					break;
+// 				}
+// 			}
+// 			System.out.println(topBlock);
+// 		}
+	}
+	
 	public static void clearArrays() {
-		for (int i = 0; i < 501; i++) {
-			for (int j = 0; j < 6; j++) {
-				cubes[i][j] = 0;
-				parent[i][j] = 0;
-			}
-			for (int j = 0; j < 101; j++) {
-				for (int k = 0; k < 6; k++) {
-					stack[i][j][k] = 0;
-				}
-			}
-		}
+// 		for (int i = 0; i <= numCubes; i++) {
+// 			for (int j = 0; j <= numCubes; j++) {
+// 				for (int k = 0; k < 6; k++) {
+// 					parent[i][j][k] = 0;
+// 				}
+// 			}
+// 			for (int j = 0; j < 101; j++) {
+// 				for (int k = 0; k < 6; k++) {
+// 					stack[i][j][k] = 0;
+// 				}
+// 			}
+// 			for (int j = 0; j < 6; j++) {
+// 				cubes[i][j] = 0;
+// 			}
+// 		}
 	}
 	
 	public static int opposite(int i) {
@@ -91,17 +145,4 @@ class Main {
 		
 		return 4;
 	}
-}
-
-enum Face {
-    front(0), back(1), left(2), right(3), top(4), bottom(5);
-
-    private final int value;
-    private Face(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
