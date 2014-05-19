85c7f19ac2554d7afb769049124864f4ae4e3e55
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-19 15:40:54 -0400

    Implemented a recursive solution using a linked list to store the result. AC

--- 291/Main.java
+++ 291/Main.java
@@ -1,13 +1,46 @@
 //--UVA #291 - House of Santa Clause
 
+import java.util.*;
+
 class Main {
+
+	public static int[][] graph = new int[][] {
+							{ 0, 0, 0, 0, 0, 0 }
+							,{ 0, 0, 1, 1, 0, 1 }
+							,{ 0, 1, 0, 1, 0, 1 }
+							,{ 0, 1, 1, 0, 1, 1 }
+							,{ 0, 0, 0, 1, 0, 1 }
+							,{ 0, 1, 1, 1, 1, 0 } };
+	public static LinkedList<Integer> result = new LinkedList<Integer>();
+
 	public static void main(String[] args) {
-		int[][] graph = new int[][] {
-			{ 0, 1, 1, 1, 1, 0 }
-			,{ 0, 0, 0, 1, 0, 1 }
-			,{ 0, 1, 1, 0, 1, 1 }
-			,{ 0, 1, 0, 1, 0, 1 }
-			,{ 0, 0, 1, 1, 0, 1 }
-			,{ 0, 0, 0, 0, 0, 0 } };
+		result.add(1);
+		makeConnections(1);
+	}
+	
+	public static void makeConnections(int source) {
+		if (result.size() > 8)
+			return;
+	
+		for (int target = 1; target <= 5; target++) {
+			if (graph[source][target] == 1) {
+				graph[source][target] = 0;
+				graph[target][source] = 0;
+				result.add(target);
+				
+				if (result.size() == 9) {
+					for (Integer i : result)
+						System.out.print(i);
+					System.out.println();
+				}
+				
+				makeConnections(target);
+				
+				graph[source][target] = 1;
+				graph[target][source] = 1;
+				result.removeLast();
+			}
+			
+		}
 	}
 }
