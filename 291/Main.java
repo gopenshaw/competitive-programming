8b7a9ca107a05c7cee5c5b990828e1f8dcfcf18e
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-19 15:11:50 -0400

    Set up initial graph

---
+++ 291/Main.java
@@ -0,0 +1,13 @@
+//--UVA #291 - House of Santa Clause
+
+class Main {
+	public static void main(String[] args) {
+		int[][] graph = new int[][] {
+			{ 0, 1, 1, 1, 1, 0 }
+			,{ 0, 0, 0, 1, 0, 1 }
+			,{ 0, 1, 1, 0, 1, 1 }
+			,{ 0, 1, 0, 1, 0, 1 }
+			,{ 0, 0, 1, 1, 0, 1 }
+			,{ 0, 0, 0, 0, 0, 0 } };
+	}
+}
