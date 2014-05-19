1e616c16ad5e6ccf17cf320db896abbc9e4136a0
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-19 10:44:30 -0400

    Wrote up general problem strategy

---
+++ 10534/Main.java
@@ -0,0 +1,7 @@
+//--UVA 10534
+
+//--Problem strategy:
+//--Given n elements, we must find the index i such that
+//--the max value in the LIS from 0 to i is equal to
+//--the LIS from n-1 to i.
+//--Once this index if found, we simply print both LIS
