16b5a2d8885bbee6230304e25d2bec6fa4199a30
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-19 21:03:51 -0400

    Train object will hold min and max weights currently on the train.

--- 11456/Main.java
+++ 11456/Main.java
@@ -2,12 +2,21 @@
 
 import java.util.*;
 
+class Train {
+	int min;
+	int max;
+}
+
 class Main {
 
 	public static int[] weight = new int[2000];
+	public static Train[] train = new Train[2001];
 	public static int numCars;
 
 	public static void main(String[] args) {
+		for (int i = 0; i <= 2001; i++) {
+			train[i] = new Train();
+		}
 		Scanner conIn = new Scanner(System.in);
 		int numCases = conIn.nextInt();
 		for (int i = 0; i < numCases; i++) {
