594afc6dea2d88fe31c4e719df0e40d0212593f5
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-21 10:35:58 -0400

    Refactor to compute lis and lds in the same loop

--- 11790/Main.java
+++ 11790/Main.java
@@ -13,81 +13,77 @@ class Main {
 	//--L[] holds the length
 	public static int[] L = new int[1000000];
 	
-	//--M[i] will hold the max length for
-	//--a sequence that ends with element i
-	public static int[] M = new int[1000000];
+	//--inc and dec will hold max length for increasing
+	//--and decreasing sequences, respectively, that end
+	//--with element i
+	public static int[] inc = new int[1000000];
+	public static int[] dec = new int[1000000];
 	
 	public static int numBuildings;
+	public static int lis;
+	public static int lds;
 
 	public static void main(String[] args) {
 		Scanner conIn = new Scanner(System.in);
 		int numCases = conIn.nextInt();
 		for (int n = 0; n < numCases; n++) {
+			//--Get input
 			numBuildings = conIn.nextInt();
 			for (int i = 0; i < numBuildings; i++)
 				H[i] = conIn.nextInt();
 			for (int i = 0; i < numBuildings; i++)
 				L[i] = conIn.nextInt();
-			int increasing = LIS();
-			int decreasing = LDS();
 			
-			if (increasing > decreasing) {
+			//--Solve problem
+			solveProblem();
+			
+			//--Print results
+			if (lis > lds) {
 				System.out.println("Case " + (n + 1)
-					+ ". Increasing (" + increasing + "). Decreasing ("
-					+ decreasing + ").");
+					+ ". Increasing (" + lis + "). Decreasing ("
+					+ lds + ").");
 			}
 			else {
 				System.out.println("Case " + (n + 1)
-					+ ". Decreasing (" + decreasing + "). Increasing ("
-					+ increasing + ").");
+					+ ". Decreasing (" + lds + "). Increasing ("
+					+ lis + ").");
 			}
 		}
 	}
 	
-	public static int LIS() {
-		int length = init();
+	public static void solveProblem() {
+		init();
 		
 		//--for every building, check every building to its left
 		for (int i = 1; i < numBuildings; i++) {
 			for (int j = 0; j < i; j++) {
 				if (H[j] < H[i]) {
-					int newLength = L[i] + M[j];
-					if (newLength > M[i])
-						M[i] = newLength;
-					if (M[i] > length)
-						length = M[i];
+					int newLength = L[i] + inc[j];
+					if (newLength > inc[i])
+						inc[i] = newLength;
+					if (inc[i] > lis)
+						lis = inc[i];
+				}
+				else if (H[j] > H[i]) {
+					int newLength = L[i] + dec[j];
+					if (newLength > dec[i])
+						dec[i] = newLength;
+					if (dec[i] > lds)
+						lds = dec[i];
 				}
 			}
 		}
-		return length;
 	}
 	
-	public static int LDS() {
-		int length = init();
-		
-		//--for every building, check every building to its left
-		for (int i = 1; i < numBuildings; i++) {
-			for (int j = 0; j < i; j++) {
-				if (H[j] > H[i]) {
-					int newLength = L[i] + M[j];
-					if (newLength > M[i])
-						M[i] = newLength;
-					if (M[i] > length)
-						length = M[i];
-				}
-			}
-		}
-		return length;
-	}
-	
-	public static int init() {
-		//--sets up M array and returns longest sequence of length one
+	public static void init() {
+		//--sets up inc and dec arrays and 
+		//--sets lis and lds to longest sequence of length one
 		int length = 0;
 		for (int i = 0; i < numBuildings; i++) {
-			M[i] = L[i];
+			inc[i] = dec[i] = L[i];
 			if (L[i] > length)
 				length = L[i];
 		}
-		return length;
+		lis = lds = length;
 	}
 }
