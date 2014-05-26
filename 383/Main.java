ac2c72c079fad4c8731929aac5b58f9ea96d1f87
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-26 12:17:53 -0400

    Getting input

---
+++ 383/Main.java
@@ -0,0 +1,45 @@
+// 
+// UVA #383 - Shipping Routes
+// Garrett Openshaw
+//
+
+import java.util.*;
+
+class Main {
+
+	public static HashMap<String, Integer> warehouseMap = new HashMap<String, Integer>();
+	public static boolean[][] graph = new boolean[30][30];
+
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+		int numCases = conIn.nextInt();
+		
+		for (int n = 1; n <= numCases; n++) {
+			int numWarehouses = conIn.nextInt();
+			int numLegs = conIn.nextInt();
+			int numRequests = conIn.nextInt();
+			
+			for (int i = 0; i < numWarehouses; i++) {
+				warehouseMap.put(conIn.next(), i);
+			}
+			
+			for (int i = 0; i < numLegs; i++) {
+				String a = conIn.next();
+				String b = conIn.next();
+				graph[warehouseMap.get(a)][warehouseMap.get(b)] = true;
+			}
+			
+			for (int i = 0; i < numRequests; i++) {
+				int cost = conIn.nextInt();
+				String a = conIn.next();
+				String b = conIn.next();
+				int distance = bfs(warehouseMap.get(a), warehouseMap.get(b));
+			}
+		}
+	}
+	
+	public static int bfs(int source, int dest) {
+		return -1;
+	}
+
+}
