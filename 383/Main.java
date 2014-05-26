d870bbe46ee43d605c5c31f3305527aaa72efee2
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-26 12:33:24 -0400

    Implemented BFS

--- 383/Main.java
+++ 383/Main.java
@@ -5,10 +5,21 @@
 
 import java.util.*;
 
+class Leg {
+	public int distance;
+	public int node;
+	
+	public Leg(int node, int distance) {
+		this.node = node;
+		this.distance = distance;
+	}
+}
+
 class Main {
 
 	public static HashMap<String, Integer> warehouseMap = new HashMap<String, Integer>();
 	public static boolean[][] graph = new boolean[31][31];
+	public static boolean [] wasVisited = new boolean[31];
 
 	public static void main(String[] args) {
 		Scanner conIn = new Scanner(System.in);
@@ -35,11 +46,31 @@ class Main {
 				String a = conIn.next();
 				String b = conIn.next();
 				int distance = bfs(warehouseMap.get(a), warehouseMap.get(b));
+				System.out.println(distance);
 			}
 		}
 	}
 	
 	public static int bfs(int source, int dest) {
+		//--Will return legs traveled to get from source to destination.
+		//--Will return -1 if no route is possible
+		LinkedList<Leg> q = new LinkedList<Leg>();
+		
+		q.add(new Leg(source, 0));
+		
+		while (!q.isEmpty()) {
+			Leg current = q.poll();
+			if (current.node == dest)
+				return current.distance;
+		
+			for (int i = 0; i < 31; i++) {
+				if (graph[current.node][i] && !wasVisited[i]) {
+					wasVisited[i] = true;
+					q.add(new Leg(i, current.distance + 1));
+				}
+			}
+		}
+		
 		return -1;
 	}
 
