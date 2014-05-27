1549436a2498a1c690ecab60509fc6a2c8b7e938
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-27 14:55:11 -0400

    Getting input. Words before the star need to be added to the dictionary.

--- 429/Main.java
+++ 429/Main.java
@@ -3,7 +3,7 @@
 // Garrett Openshaw
 // 
 
-import java.util.*
+import java.util.*;
 
 class Main {
 
@@ -12,18 +12,32 @@ class Main {
 		int numCases = conIn.nextInt();
 		
 		for (int n = 0; n < numCases; n++) {
-			String input;
-			do {
+			String input = conIn.next();
+			while (!input.equals("*")) {
+				//--Add words to dictionary
 				input = conIn.next();
-				
-				
-				
-				
-			}	while (input.length() != 0);
-			
-			if (n != numCases - 1) {
-				System.out.println();
 			}
-		}	
-	}	
+			
+			conIn.nextLine();
+			String[] words = new String[2];
+			
+			while (conIn.hasNext()) {
+				input = conIn.nextLine();
+				if (input.length() == 0)
+					break;
+				
+				words = input.split(" ");
+				System.out.println(words[0] + " " + words[1] + " "
+									+ getDistance(words[0], words[1]));
+				
+			}
+			
+			if (n != numCases - 1)
+				System.out.println();	
+		}
+	}
+	
+	public static int getDistance(String word1, String word2) {
+		return -1;
+	}
 }
