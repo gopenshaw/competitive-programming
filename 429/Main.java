ecb3ba374d43b2988ce4e807324d4cef6496236e
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-27 14:31:19 -0400

    Basic setup

---
+++ 429/Main.java
@@ -0,0 +1,29 @@
+// 
+// UVA 429 - Word Transformation
+// Garrett Openshaw
+// 
+
+import java.util.*
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+		int numCases = conIn.nextInt();
+		
+		for (int n = 0; n < numCases; n++) {
+			String input;
+			do {
+				input = conIn.next();
+				
+				
+				
+				
+			}	while (input.length() != 0);
+			
+			if (n != numCases - 1) {
+				System.out.println();
+			}
+		}	
+	}	
+}
