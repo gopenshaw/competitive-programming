6b79a15a885bdffd6b05a23533b12251cc1946b8
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-31 18:02:23 -0400

    Added class to handle disjoint sets

--- 908/Main.java
+++ 908/Main.java
@@ -22,6 +22,41 @@ class Edge implements Comparable<Edge>{
 	}
 }
 
+class DisjointSets {
+
+	private int[] array;
+
+	public DisjointSets(int numElements) {
+		array = new int [numElements];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = -1;
+		}
+	}
+
+	public void union(int root1, int root2) {
+		if (array[root2] < array[root1]) {
+			array[root1] = root2;             // root2 is taller; make root2 new root
+		} 
+		else {
+			if (array[root1] == array[root2]) {
+				array[root1]--;            // Both trees same height; new one is taller
+			}
+			array[root2] = root1;       // root1 equal or taller; make root1 new root
+		}
+	}
+
+	public int find(int x) {
+		if (array[x] < 0) {
+			return x;                         // x is the root of the tree; return it
+		}
+		else {
+	      // Find out who the root is; compress path by making the root x's parent.
+			array[x] = find(array[x]);
+			return array[x];                                       // Return the root
+		}
+	}
+}
+
 class Main {
 
 	public static PriorityQueue<Edge> q = new PriorityQueue<Edge>();
