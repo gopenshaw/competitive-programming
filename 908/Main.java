1c54d675890c3234c915ec9276a5b1b070b7b790
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-05-31 18:01:10 -0400

    Added comparable Edge class

--- 908/Main.java
+++ 908/Main.java
@@ -5,11 +5,33 @@
 
 import java.util.*;
 
+class Edge implements Comparable<Edge>{
+	int source;
+	int dest;
+	int cost;
+	
+	public Edge(int source, int dest, int cost) {
+		this.source = source;
+		this.dest = dest;
+		this.cost = cost;
+	}
+	
+	@Override
+	public int compareTo(Edge e) {
+		return this.cost - e.cost;
+	}
+}
+
 class Main {
+
+	public static PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+
 	public static void main(String[] args) {
 		Scanner conIn = new Scanner(System.in);
 		
 		while (conIn.hasNext()) {
+			q.clear();
+		
 			int numSites = conIn.nextInt();
 			int originalCost = 0;
 			for (int i = 0; i < numSites - 1; i++) {
@@ -25,6 +47,7 @@ class Main {
 				int source = conIn.nextInt();
 				int dest = conIn.nextInt();
 				int cost = conIn.nextInt();
+				q.add(new Edge(source, dest, cost));
 			}
 			
 			int numOriginal = conIn.nextInt();
@@ -32,6 +55,7 @@ class Main {
 				int source = conIn.nextInt();
 				int dest = conIn.nextInt();
 				int cost = conIn.nextInt();
+				q.add(new Edge(source, dest, cost));
 			}
 		}
 	}
