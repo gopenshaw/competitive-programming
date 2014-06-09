cd3e489e2503cbd59a5ef6066d715656cc93a3ed
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-06-09 17:38:24 -0400

    Implemented a knapsack solution

---
+++ 562/Main.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Main {
+
+	public static int[] value = new int[101];
+	public static int[][] V = new int[101][25000];
+
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+		int numCases = conIn.nextInt();
+		
+		for (int n = 0; n < numCases; n++) {
+			int numCoins = conIn.nextInt();
+			int totalValue = 0;
+			for (int i = 1; i <= numCoins; i++) {
+				int coin = conIn.nextInt();
+				totalValue += coin;	
+				value[i] = coin;		
+			}
+			
+			int maxValue = totalValue / 2;	
+
+			for (int i = 0; i <= maxValue; i++) {
+				V[0][i] = 0;
+			}
+			
+			int offset = totalValue % 2 == 0 ? 0 : 1;
+			
+// 			System.out.println("Total is " + totalValue);
+// 			System.out.println("Our target was " + maxValue);
+// 			System.out.println("We filled " + knapSack(numCoins, maxValue));
+			System.out.println(totalValue - 2 * knapSack(numCoins, maxValue));
+		}
+	}
+	
+	public static int knapSack(int numCoins, int maxValue) {
+		for (int i = 1; i <= numCoins; i++) {
+			for (int j = 0; j <= maxValue; j++) {
+				int currentValue = value[i];
+				if (currentValue <= j) {
+					V[i][j] = 
+						Math.max(V[i - 1][j], 
+								currentValue + V[i - 1][j - currentValue]);
+				}
+				else 
+					V[i][j] = V[i - 1][j];
+			}
+		} 
+			
+		return V[numCoins][maxValue]; 
+	}
+}
