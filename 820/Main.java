72ee62ee883509567f9fbb01c5e54f87d932c335
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-07-04 14:58:49 -0400

    getting some input

--- 820/Main.java
+++ 820/Main.java
@@ -2,8 +2,20 @@
 // UVA #820 - Internet Bandwidth
 // Garrett Openshaw
 
-import java.util.*
+import java.util.*;
 
 class Main {
-	
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+
+		int numNodes = conIn.nextInt();
+
+		while (numNodes != 0) {
+
+
+
+
+			numNodes = conIn.nextInt();
+		}
+	}
 }
