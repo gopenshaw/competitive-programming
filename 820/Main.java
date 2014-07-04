7d7e0090f09c9f568d3c769ae66ad824fb566436
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-07-04 15:01:52 -0400

    Getting more input

--- 820/Main.java
+++ 820/Main.java
@@ -12,7 +12,18 @@ class Main {
 
 		while (numNodes != 0) {
 
+			int source = conIn.nextInt();
+			int destination = conIn.nextInt();
 
+			int numConnections = conIn.nextInt();
+
+			for (int i = 0; i < numConnections; i++) {
+				int node1 = conIn.nextInt();
+				int node2 = conIn.nextInt();
+				int bandwidth = conIn.nextInt();
+
+				
+			}
 
 
 			numNodes = conIn.nextInt();
