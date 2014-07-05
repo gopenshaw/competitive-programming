f12905cc0436bbd2166d6e472d659df31893748a
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-07-05 13:10:20 -0400

    basic input and output

---
+++ 10779/Main.java
@@ -0,0 +1,39 @@
+//
+// UVA #10779 - Collector's Problem
+// Garrett Openshaw
+//
+
+import java.util.*;
+
+class Main {
+	
+	public static int[][] stickers = new int[10][25];
+	public static int numPeople;
+	public static int numStickers;
+
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+		int numCases = conIn.nextInt();
+
+		for (int n = 1; n <= numCases; n++) {
+			numPeople = conIn.nextInt();
+			numStickers = conIn.nextInt();
+
+			for (int i = 0; i < numPeople; i++) {
+				int count = conIn.nextInt();
+				for (int c = 0; c < count; c++) {
+					int value = conIn.nextInt();
+					stickers[i][value]++;
+				}
+			}
+
+			int maxStickers = maxStickers();
+
+			System.out.println("Case #" + n + ": " + maxStickers);
+		}
+	}
+
+	public static int maxStickers() {
+		return 0;
+	}
+}
