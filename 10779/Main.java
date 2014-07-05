33eae16c62610f11a1ebd7d791ce7fff395256a2
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-07-05 14:19:24 -0400

    stubbed out methods for algorithm. I will create edges to represent a sticker that could be moved from one person to another. Once all of the edges are created, I will try to find cycles that start and end at Bob. When there are no cycles remaining, the process is complete.

--- 10779/Main.java
+++ 10779/Main.java
@@ -5,6 +5,27 @@
 
 import java.util.*;
 
+class Edge {
+	public int source;
+	public int destination;
+	public int sticker;
+
+	public Edge(int source, int destination, int stickers) {
+		this.source = source;
+		this.destination = destination;
+		this.sticker = sticker;
+	}
+}
+
+class Path {
+	public ArrayList<Edge> edges;
+
+	public Path() {
+		this.edges = new ArrayList<Edge>();
+	}
+}
+	
+
 class Main {
 	
 	public static int[][] stickers = new int[10][25];
@@ -26,10 +47,22 @@ class Main {
 					stickers[i][value]++;
 				}
 			}
+			
+			makeEdges();
+			
+			Path cycle = getCycle();
+			while (cycle != null) {
+				makeTrades(cycle);
+				cycle = getCycle();
+			}
 
-			int maxStickers = maxStickers();
+			int count = 0;
+			for (int i = 0 ; i < numStickers; i++) {
+				if (stickers[0][i] != 0)
+					count++;
+			}
 
-			System.out.println("Case #" + n + ": " + maxStickers);
+			System.out.println("Case #" + n + ": " + count);
 
 			if (n != numCases) {
 				for (int i = 0; i < numPeople; i++) {
@@ -41,7 +74,17 @@ class Main {
 		}
 	}
 
-	public static int maxStickers() {
-		return 0;
+	public static void makeEdges() {
+		return;
+	}
+
+	public static Path getCycle() {
+		//--use bfs
+
+		return null;
+	}
+
+	public static void makeTrades(Path path) {
+		return;
 	}
 }
