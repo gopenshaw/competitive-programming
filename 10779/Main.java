5e812bb3e7c2b53d5088556453e9f43c3812c98f
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-09-14 14:47:33 -0400

    Implemented the bfs
    doing stuff in a weird order...

--- 10779/Main.java
+++ 10779/Main.java
@@ -1,10 +1,31 @@
 import java.util.*;
 
+class Edge {
+	int source;
+	int target;
+	int capacity;
+	int flow;
+	Edge parent;
+	Edge residual;
+
+	public Edge(int capacity) {
+		this.capacity = capacity;
+	}
+}
+
+class Vertex {
+	ArrayList<Edge> fromHere = new ArrayList<Edge>();
+}
+
 class Main {
 	static Scanner input;
 	static int numCases;
 	static int numPeople;
 	static int numStickers;
+	static int numVertexes;
+	static int sourceVertex;
+	static int targetVertex;
+	static Vertex[] vertexArray;
 	static int[][] stickerCount;
 
 	static void getInput() {
@@ -25,7 +46,39 @@ class Main {
 	}
 
 	static void doMaxFlowAndPrintResult() {
-		
+		Edge finalEdge = bfs();
+	}
+
+	static Edge bfs() {
+		Edge finalEdge = null;
+		LinkedList<Edge> q = new LinkedList<Edge>();
+		boolean visited[] = new boolean[numVertexes];
+		visited[sourceVertex] = true;
+
+		for (Edge edge : vertexArray[sourceVertex].fromHere) {
+			if (edge.flow < edge.capacity) {
+				q.add(edge);
+			}
+		}
+
+		while (!q.isEmpty()) {
+			Edge current = q.poll();	
+			if (current.target == targetVertex) {
+				finalEdge = current;
+				break;	
+			}
+			visited[current.source] = true;
+			
+			for (Edge edge : vertexArray[current.target].fromHere) {
+				if (!visited[edge.target]
+					&& edge.flow < edge.capacity) {
+					edge.parent = current;
+					q.add(edge);
+				}
+			}
+		}
+
+		return finalEdge;
 	}
 
 	public static void main(String[] args) {
