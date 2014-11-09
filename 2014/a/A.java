650facdff301ec97f3de45de28641361725bbe14
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-11-09 15:51:40 -0500

    Program handles sample input
    Need to support for a negative a_0

---
+++ 2014/a/A.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+class A {
+	public static void main(String[] args) {
+		Scanner conIn = new Scanner(System.in);
+		int caseNum = 0;
+		int n1 = conIn.nextInt();
+		int n2 = conIn.nextInt();
+		while (n1 != 0 || n2 != 0) {
+			ArrayList<Integer> conFrac1 = new ArrayList<Integer>();
+			for (int i = 0; i < n1; i++) {
+				conFrac1.add(conIn.nextInt());
+			}
+			ArrayList<Integer> conFrac2 = new ArrayList<Integer>();
+			for (int i = 0; i < n2; i++) {
+				conFrac2.add(conIn.nextInt());
+			}
+			Fraction r1 = convertToFraction(conFrac1, 0);
+			Fraction r2 = convertToFraction(conFrac2, 0);
+//			System.out.printf("n1:%d,d1:%d\nn2:%d,d2:%d\n",
+//				r1.num,r1.den,r2.num,r2.den);
+
+			printList(convertToContinuousFraction(r1.add(r2)));
+			printList(convertToContinuousFraction(r1.subtract(r2)));
+			printList(convertToContinuousFraction(r1.multiply(r2)));
+			printList(convertToContinuousFraction(r1.divide(r2)));
+
+			n1 = conIn.nextInt();
+			n2 = conIn.nextInt();
+		}
+	}
+
+	static Fraction convertToFraction(ArrayList<Integer> list, int index) {
+		Fraction value = new Fraction(list.get(index), 1L);
+		if (index == list.size() - 1)
+			return value;
+
+		return value.add(convertToFraction(list, index + 1).inverse());
+	}
+
+	static ArrayList<Long> convertToContinuousFraction(Fraction f) {
+		ArrayList<Long> result = new ArrayList<Long>();
+		long value = f.num / f.den;
+		result.add(value);
+		Fraction rem = f.subtract(new Fraction(value, 1L));
+		while (rem.num != 0) {
+			value = rem.den / rem.num;
+			result.add(value);
+			rem = rem.inverse().subtract(new Fraction(value, 1L));
+		}
+
+		return result;
+	}
+
+	static void printList(ArrayList<Long> list) {
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i));
+			if (i != list.size() - 1)
+				System.out.print(" ");
+
+		}
+
+		System.out.println();
+	}
+}
+
+class Fraction {
+	long num;
+	long den;
+
+	Fraction(long num, long den) {
+		this.num = num;
+		this.den = den;
+	}
+
+	Fraction add(Fraction f) {
+		if (den == f.den)
+			return new Fraction(num + f.num, den);
+
+		return new Fraction(num * f.den + f.num * den, f.den * den);
+	}
+
+	Fraction subtract(Fraction f) {
+		if (den == f.den)
+			return new Fraction(num - f.num, den);
+
+		return new Fraction(num * f.den - f.num * den, f.den * den);
+	}
+
+	Fraction multiply(Fraction f) {
+		return new Fraction(num * f.num, den * f.den);
+	}
+
+	Fraction divide(Fraction f) {
+		return multiply(f.inverse());
+	}
+
+	Fraction inverse() {
+		return new Fraction(den, num);
+	}
+}
