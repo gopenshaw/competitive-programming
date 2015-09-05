4d20e6e8bb54f3eeadac9731129ea08612dfd3f8
gopenshaw <garrett.j.openshaw@gmail.com>
2015-09-05 12:52:41 -0400

    Handling cases with 9's. Code looks ugly

--- spoj/palin/Main.java
+++ spoj/palin/Main.java
@@ -7,24 +7,61 @@ class Main {
         for (int i = 0; i < numCases; i++) {
             char[] n = conIn.next().toCharArray();
             boolean larger = false;
-            for (int j = 0; j < n.length / 2; j++) {
-                int last = n.length - j - 1;
-                if (n[j] != n[last]) {
-                    larger = true;
-                    n[last] = (char)(n[j] + 1);
-                    n[j]++;
+            for (int j = (n.length - 1) / 2; j >= 0; j--) {
+                int pair = n.length - j - 1;
+                if (pair == j) continue;
+                if (n[j] != n[pair]) {
+                    if (larger) {
+                        n[pair] = n[j];
+                    }
+                    else {
+                        larger = true;
+                        n[pair] = (char)(n[j] + 1);
+                        n[j]++;
+                    }
                 }
             }
 
+            //--handle a string of 9's separately
+            boolean special = false;
+
+            //--The number was already a palindrome
             if (!larger) {
                 //--we may have to change 1 or 2 digits
                 //  at the center of the number
+                int mid = (n.length - 1) / 2;
+                while (mid >= 0 && n[mid] == '9') {
+                    if (n.length % 2 == 0) {
+                        n[mid] = '0';
+                    }
 
-                //--this change may trigger a carry, which
-                //  would affect other numbers
+                    n[n.length - mid - 1] = '0';
+                    mid--;
+
+                    special = mid < 0;
+                }
+                
+                if (special) {
+                    int zeroLength = n.length - 1;
+                    System.out.print(1);
+                    for (int j = 0; j < zeroLength; j++)
+                        System.out.print(0);
+                    
+                    System.out.println(1);
+                }
+                else {
+                    if (n.length % 2 == 0
+                        || mid != (n.length - 1) / 2) {
+                        n[n.length - mid - 1]++;
+                    }
+
+                    n[mid]++;
+                }
             }
             
-            System.out.println(n);
+            if (!special) {
+                System.out.println(n);
+            }
         }
     }
 }
