5c13acb9f05a1ddf2e35e04f28e97aa9e1dc45c2
gopenshaw <garrett.j.openshaw@gmail.com>
2015-10-20 13:23:21 -0400

    Arbitrary # of '*' and '+' in a parens

--- spoj/onp/main.cpp
+++ spoj/onp/main.cpp
@@ -12,30 +12,64 @@ bool is_operand(char c) {
   || c == '^';
 }
 
+int get_priority(char c) {
+  switch (c) {
+  case '^': return 5; break;
+  case '/': return 4; break;
+  case '*': return 3; break;
+  case '-': return 2; break;
+  default: return 1;
+  }
+}
+
+struct Operator {
+  char c;
+  int level;
+
+  Operator(char c, int level) : c(c), level(level)
+  {
+  }
+};
+
 int main()
 {
   int N;
   cin >> N;
   for (int i = 0; i < N; i++) {
     string s;
-    stack<char> c;
+    stack<Operator> ops;
     cin >> s;
+    int level = 0;
     for (int i = 0; i < s.length(); i++) {
       if (s[i] == '(') {
-
+        level++;
       }
       else if (s[i] == ')') {
-        if (c.size() != 0) {
-          cout << c.top();
-          c.pop();
+        level--;
+        while (ops.size() > 0
+              && ops.top().level > level) {
+          cout << ops.top().c;
+          ops.pop();
         }
       }
       else if (is_operand(s[i])) {
-        c.push(s[i]);
+        if (ops.size() > 0
+            && ops.top().level == level
+            && get_priority(ops.top().c) > get_priority(s[i])) {
+          cout << ops.top().c;
+          ops.pop();
+        }
+
+        ops.push(Operator(s[i], level));
       }
       else {
         cout << s[i];
       }
+    }
+
+    while (ops.size() != 0) {
+      cout << ops.top().c;
+      ops.pop();
     }
 
     cout << "\n";
